6e197c8556758a22c2bbf952ce37804906a4c3a9
Crhistian Meza <crhistianmezas@gmail.com>
2026-01-02 04:54:33 -0500

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel/serverless'; // 👈 CAMBIO CRÍTICO: Usa el adaptador de Vercel
 
 export default defineConfig({
-  output: 'server', // 👈 VITAL para que Prisma funcione en tiempo real
+  output: 'hybrid', // 👈 VITAL para que Prisma funcione en tiempo real
   adapter: vercel(), // 👈 Esto le dice a Vercel exactamente cómo servir la web
   vite: {
     plugins: [tailwindcss()]
