8d6b24e3c3c80254917308dc47bb4e1c4d2e47cd
Crhistian Meza <crhistianmezas@gmail.com>
2026-01-03 01:14:38 -0500

    fix: externalize prisma client to fix vite warning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless'; // 👈 El adaptador vital
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'server', // 👈 OBLIGATORIO: Le dice a Astro "No hagas una web estática, haz una app real"
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
   vite: {
     plugins: [tailwindcss()],
     ssr: {
-      noExternal: ['@prisma/client'] // Evita errores de Prisma en producción
+      external: ['@prisma/client'] 
     }
   }
 });
