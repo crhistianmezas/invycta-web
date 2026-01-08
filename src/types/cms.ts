618c508ddcd50b61fad51148d41d43ffea0157bc
Crhistian Meza <crhistianmezas@gmail.com>
2026-01-08 01:54:02 -0500

    fixed

--- src/types/cms.ts
+++ src/types/cms.ts
@@ -11,3 +11,30 @@ export interface WebContent {
   ctaLink?: string;
   displayOrder: number;
 }
+
+export interface Product {
+  id: string;
+  name: string;
+  description?: string;
+  image?: string;
+  slug?: string;
+  price?: number;
+  categoryId?: string;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  slug: string;
+  description?: string;
+  products?: Product[];
+}
+
+export interface Solution {
+  id: string;
+  name: string;
+  slug: string;
+  description?: string;
+  image?: string;
+  products?: Product[];
+}
